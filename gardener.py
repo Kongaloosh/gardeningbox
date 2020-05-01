22bcb4400288aa6d200a7851e92d9e1a745abab0
kongaloosh <hi@kongaloosh.com>
2020-04-30 18:13:47 -0600

    Gardener reads data and inserts it into a postgres database.

--- gardener.py
+++ gardener.py
@@ -1,3 +1,5 @@
+import Adafruit_DHT
+import spidev  # To communicate with SPI devices
 import configparser
 import psycopg2
 import time
@@ -10,7 +12,24 @@ HOST = config["POSTGRES"]['host']
 PORT = config["POSTGRES"]['port']
 DATABASE = config["POSTGRES"]['database']
 
+spi = spidev.SpiDev()  # Created an object
+spi.open(0, 0)
+
+
+def analog_input(channel):
+    spi.max_speed_hz = 1350000
+    adc = spi.xfer2([1, (8 + channel) << 4, 0])
+    data = ((adc[1] & 3) << 8) + adc[2]
+    return data
+
+
+def read_moistness(channel=0):
+    temp = analog_input(channel)
+    return (temp - 309.0) / (561.0 - 309.0)
+
+
 connection = None
+cursor = None
 try:
     connection = psycopg2.connect(user=USER,
                                   password=PASSWORD,
@@ -26,7 +45,7 @@ except (Exception, psycopg2.Error) as error:
     print("Error while connecting to PostgreSQL", error)
 
 finally:
-    if(connection):
+    if connection:
         cursor.close()
         connection.close()
         print("PostgreSQL connection is closed")
@@ -34,9 +53,29 @@ finally:
 
 def tend():
     """Fetches sensor readings and adds them to the dbms"""
-    pass
+    moistness = read_moistness()
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+    img_loc = None
+
+    try:
+        connection = psycopg2.connect(user=USER,
+                                      password=PASSWORD,
+                                      host=HOST,
+                                      port=PORT,
+                                      database=DATABASE
+                                      )
+
+
+
+        cursor = connection.cursor()
+        postgres_insert_query = """ INSERT INTO garden (time, humidity, temperature, soil_moisture, img_loc) VALUES (%s,%s,%s,%s)"""
+        record_to_insert = (time.time(), humidity, temperature, moistness, img_loc)
+        cursor.execute(postgres_insert_query, record_to_insert)
+    except (Exception, psycopg2.Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+
 
 if __name__ == "__main__":
     while True:
-        tend()              # collect data and record it
-        time.sleep(60)      # wait 60 seconds for next recording
+        tend()  # collect data and record it
+        time.sleep(60)  # wait 60 seconds for next recording
